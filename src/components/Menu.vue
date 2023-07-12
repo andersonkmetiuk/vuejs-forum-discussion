eb733aa0fbe8ead0e5c91086e1b1b79e73521ada
Anderson Kmetiuk <58405194+andersonkmetiuk@users.noreply.github.com>
2023-07-12 20:39:46 -0300

    first commit

---
+++ src/components/Menu.vue
@@ -0,0 +1,12 @@
+<template>
+    <h1>Menu</h1>
+       <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/login">Login</RouterLink>
+      </nav>
+</template>
+
+<script setup>
+import { RouterLink } from 'vue-router'
+</script>
---
+++ src/views/LoginView.vue
@@ -0,0 +1,12 @@
+<template>
+  <h1>Login View</h1>
+  <form action="">
+    <input type="text" placeholder="Email">
+    <input type="text" placeholder="Password">
+    <button type="submit">Login</button>
+  </form>
+</template>
+
+<script setup>
+    console.log('login');
+</script>
